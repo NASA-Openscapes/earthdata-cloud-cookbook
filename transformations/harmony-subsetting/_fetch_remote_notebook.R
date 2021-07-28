cce61247fe65826c97d5128910c7c9d873b52255
jules32 <lowndes@nceas.ucsb.edu>
2021-07-27 18:20:22 -0700

    committing attempts at environment.yml

--- transformations/harmony-subsetting/_fetch_remote_notebook.R
+++ transformations/harmony-subsetting/_fetch_remote_notebook.R
@@ -1,4 +1,4 @@
-## _site.R
+## _fetch_remote_notebook.R
 ## A script to fetch remote documents we want to include in the Cookbook.
 ## These remote resources can be included as a result of this intentional import operation.
 ## They will be converted to .qmd files so that they can be included as "child" documents.
@@ -17,7 +17,7 @@ to_copy <- c(
   "https://raw.githubusercontent.com/podaac/tutorials/master/notebooks/harmony%20subsetting/environment.yml"
   )
 
-local_dir <- "transformations"
+local_dir <- "transformations/harmony-subsetting"
 
 ## loop through and copy files: .ipynb gets a "_" prefix so that it's not served by quarto ----
 for (f in to_copy) { # f = "https://raw.githubusercontent.com/podaac/tutorials/master/notebooks/harmony%20subsetting/Harmony%20L2%20Subsetter.ipynb"
@@ -35,20 +35,54 @@ for (f in to_copy) { # f = "https://raw.githubusercontent.com/podaac/tutorials/m
     ## save local copy with f_local prefix
     f_local_dir <- paste(local_dir, f_local, sep="/")
     readr::write_lines(f_web, file = here(f_local_dir), append = FALSE)
-    message(sprintf('saving %s', f_local_dir))
+    message(sprintf('Saving %s', f_local_dir))
     
-    ## if .ipynb, convert to .qmd
+    ## if .ipynb, convert to .qmd: use system() to call bash commands
     if (tools::file_ext(f) == 'ipynb') {
       f_local_dir <- paste0(tools::file_path_sans_ext(f_local_dir))
-      
       system(sprintf(
         "quarto convert %s.ipynb --output %s.qmd", f_local_dir, f_local_dir))
-      # TODO: delete .ipynb?
+      
+      ## delete the local .ipynb (retain only local .qmd)
+      file.remove(sprintf("%s.ipynb", f_local_dir))
+      message(sprintf('Local copy deleted: %s', f_local_dir))
     }
     
   ## if RCurl::url.exists(f) returns FALSE  
   } else {
-    message(sprintf('%s does not exist', fp))
+    message(sprintf('%s does not exist', f))
   }
 }
 
+
+# ## set up the conda environment ---
+# ## use system() to run bash commands
+# 
+# ## create a new Python 3 virtual environment in the directory env
+# system(sprintf("conda env create -f %s", here(local_dir, "environment.yml")))
+# 
+# # To use the environment you need to activate it.  
+# system("conda activate papermill") #conda activate podaac-harmony
+# 
+# ## Try another way ---
+# ##from https://quarto.org/docs/getting-started/quarto-projects.html?q=renv#virtual-environments
+# 
+# ## To create a new Python 3 virtual environment in the directory env:
+# system(sprintf("conda create --prefix %s python", here(local_dir, "env")))
+# 
+# ##If this is the first time you’ve used conda in your shell, you may need to execute one of the following commands before using other conda tools:
+# system("conda init bash")
+# 
+# ## To use the environment you need to activate it, which you do as follows:
+# system("conda activate ./env")
+# 
+# ## To reproduce the environment on another machine you just pass the environment.yml file as an argument to conda create:
+# system(sprintf("conda create --prefix env -f %s", here(local_dir, "environment.yml")))
+# 
+# ## conda bash commands
+# ### https://quarto.org/docs/getting-started/quarto-projects.html?q=renv#virtual-environments
+# # conda create --prefix env python
+# # conda init bash 
+# # restart terminal
+# # conda activate ./env
+# # conda create --prefix env -f environment.yml
