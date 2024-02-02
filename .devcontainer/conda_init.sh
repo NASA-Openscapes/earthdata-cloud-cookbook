22b91bb4c1731f34c5c8e04bed707d6fa1058e45
Carl <cboettig@gmail.com>
2024-02-01 16:01:33 -0800

    revising image

---
+++ .devcontainer/conda_init.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+conda init &> /dev/null
+
--- .devcontainer/install.R
+++ .devcontainer/install.R
@@ -3,9 +3,11 @@
 
 # We could use renv.lock approach here instead, but will force re-creation of environment from scratch
 # Does not provide a good way to ensure that sf/terra/gdalcubes are installed from source while other packages can be binary
-
+install.packages("pak")
 install.packages(c("rstac", "spData", "earthdatalogin", "quarto"))
-remotes::install_github('r-tmap/tmap')
+pak::pkg_install('github::r-tmap/tmap')
 
-install.packages(c("IRkernel", "languageserver", "httpgd", "aws.s3"))
+#pak::pkg_install("httpgd")
+pak::pkg_install(c("IRkernel", "languageserver"))
 IRkernel::installspec()
+
