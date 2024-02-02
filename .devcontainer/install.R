76feab110e66a21f3c122f21e1920e5ebaeeb309
Carl <cboettig@gmail.com>
2024-02-01 19:17:01 -0800

    deps

--- .devcontainer/install.R
+++ .devcontainer/install.R
@@ -4,7 +4,7 @@
 # We could use renv.lock approach here instead, but will force re-creation of environment from scratch
 # Does not provide a good way to ensure that sf/terra/gdalcubes are installed from source while other packages can be binary
 install.packages("pak")
-install.packages(c("rstac", "spData", "earthdatalogin", "quarto"))
+pak::pkg_install(c("rstac", "spData", "earthdatalogin", "quarto", "aws.s3"))
 pak::pkg_install('github::r-tmap/tmap')
 
 #pak::pkg_install("httpgd")
