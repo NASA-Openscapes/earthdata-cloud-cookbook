eabd5f3cc728ca418d75ac18265d32b22fc27204
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2025-01-22 19:53:14 +0000

    Built site for gh-pages

--- site_libs/jquery-3.6.0/jquery-3.6.0.js
+++ site_libs/jquery-3.6.0/jquery-3.6.0.js
@@ -8615,7 +8615,7 @@ jQuery.each( [ "radio", "checkbox" ], function() {
 
 
 
-// Return jQuery for attributes-only inclusion
+// Return jQuery for attributes-only
 
 
 support.focusin = "onfocusin" in window;
